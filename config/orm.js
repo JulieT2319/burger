b297e41856c2070f6a58b90ce138733095a13157
Julie T <47932789+JulieT2319@users.noreply.github.com>
2020-02-26 10:28:33 -0700

    added insert and update functions needed to orm

--- config/orm.js
+++ config/orm.js
@@ -2,12 +2,28 @@
 const connection = require("../config/connection.js");
 
 orm = {
-  selectAll: function (tableToSelect) {
+  selectAll: function (table) {
     const query = 'SELECT * FROM ??';
-    connection.query(query, [tableToSelect], function (err, results) {
+    connection.query(query, [table], function (err, results) {
       if (err) throw err;
       console.log(results);
     })
+  },
+  insertOne: function (table, columns, values) {
+    const query = "INSERT INTO ?? (??) VALUES (?);";
+    connection.query(query, [table, columns, values],
+      function (err) {
+        if (err) throw err;
+        console.log("Your table was added to successfully!");
+      });
+  },
+  updateOne: function (table, column, value, conditionCol, conditionVal) {
+    const query = "UPDATE ?? SET ??=? WHERE ?? = ?";
+    connection.query(query, [table, column, value, conditionCol, conditionVal],
+      function (err) {
+        if (err) throw err;
+        console.log("Your table was updated successfully!");
+      });
   }
 };
 

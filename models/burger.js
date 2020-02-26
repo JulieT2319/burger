4dd05fed06b29decca109c91bcc712c98b23fccb
Julie T <47932789+JulieT2319@users.noreply.github.com>
2020-02-26 11:29:27 -0700

    added cb function to burgers model

--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,21 @@
 const orm = require("../config/orm");
 
 const burger = {
-  all: function () {
-    orm.selectAll(burgers);
+  all: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res)
+    });
   },
   add: function (value) {
-    orm.insertOne("burgers", "burger_name", value);
+    orm.insertOne("burgers", "burger_name", value, function (res) {
+      cb(res)
+    });
   },
   update: function (conditionVal) {
     //table, column, value, conditionCol, conditionVal
-    orm.updateOne("burgers", "devoured", true, "id", conditionVal);
+    orm.updateOne("burgers", "devoured", true, "id", conditionVal, function (res) {
+      cb(res)
+    });
   }
 };
 

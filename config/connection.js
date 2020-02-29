ba73059a996da510c9507e3a735a3395ff82a45c
Julie T <joules2319@gmail.com>
2020-02-29 14:28:53 -0700

    updated environment variables

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: DB_PORT,
-  user: "root",
-  password: DB_PASSWORD,
-  database: "burgers_db"
+	host: "localhost",
+	port: process.env.DB_PORT,
+	user: "root",
+	password: process.env.DB_PASSWORD,
+	database: "burgers_db"
 });
 
 // Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function(err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for ORM to use.

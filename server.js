86ef3bfc76acc716f88572ca18bb5bfb9c81b7f0
Julie T <joules2319@gmail.com>
2020-02-29 14:29:04 -0700

    added routes

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 //Server Dependencies
 const express = require("express");
-const exphbs = require("express-handlebars");
-require('dotenv').config();
+require("dotenv").config();
 
 //Setting up server
-const app = express();
 const PORT = process.env.PORT;
+const app = express();
 
 //Setting up data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Setting up handlebars
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
-app.listen(PORT, function () {
-  // Log when server has started.
-  console.log("Server listening on: http://localhost:" + PORT);
+//Setting up Routes
+const routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+app.listen(PORT, function() {
+	// Log when server has started.
+	console.log("Server listening on: http://localhost:" + PORT);
 });

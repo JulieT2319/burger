125be0564504f6efab8216ba0f5fa7ef2079ee05
Julie T <joules2319@gmail.com>
2020-02-29 19:07:22 -0700

    added delete function and cb function parameters

--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,29 @@
 const orm = require("../config/orm");
 
 const burger = {
-  all: function (cb) {
-    orm.selectAll("burgers", function (res) {
-      cb(res)
-    });
-  },
-  add: function (value) {
-    orm.insertOne("burgers", "burger_name", value, function (res) {
-      cb(res)
-    });
-  },
-  update: function (conditionVal) {
-    //table, column, value, conditionCol, conditionVal
-    orm.updateOne("burgers", "devoured", true, "id", conditionVal, function (res) {
-      cb(res)
-    });
-  }
+	all: function(cb) {
+		orm.selectAll("burgers", function(res) {
+			cb(res);
+		});
+	},
+	add: function(value, cb) {
+		orm.insertOne("burgers", "burger_name", value, function(res) {
+			cb(res);
+		});
+	},
+	update: function(conditionVal, cb) {
+		//table, column, value, conditionCol, conditionVal
+		orm.updateOne("burgers", "devoured", true, "id", conditionVal, function(
+			res
+		) {
+			cb(res);
+		});
+	},
+	delete: function(conditionVal, cb) {
+		orm.removeOne("burgers", "id", conditionVal, function(res) {
+			cb(res);
+		});
+	}
 };
 
 module.exports = burger;

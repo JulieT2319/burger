bd94d7330ee1d22914ec647e2b4722d12f250237
Julie T <joules2319@gmail.com>
2020-02-29 19:06:29 -0700

    added delete route adjusted other route parameters

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const burger = require("../models/burger");
 
 router.get("/", function(req, res) {
-	console.log("attempt root route");
 	burger.all(function(data) {
 		console.log(data);
 		let eaten = data.filter(burger => {
@@ -22,15 +21,26 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-	burger.add(req.body.name, function(result) {
+	burger.add(req.body.burger_name, function(result) {
 		// Send back the ID of the new burger
 		res.json({ id: result.insertId });
 	});
 });
 
-router.put("/api/burgers", function(req, res) {
-	burger.update(req.body.id, function(result) {
+router.put("/api/burgers/:id", function(req, res) {
+	burger.update(req.params.id, function(result) {
 		if (result.changedRows == 0) {
+			// If no rows were changed, then the ID must not exist, so 404
+			return res.status(404).end();
+		} else {
+			res.status(200).end();
+		}
+	});
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+	burger.delete(req.params.id, function(result) {
+		if (result.affectedRows == 0) {
 			// If no rows were changed, then the ID must not exist, so 404
 			return res.status(404).end();
 		} else {
